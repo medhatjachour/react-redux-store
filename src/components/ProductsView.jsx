95f9662b3f99e69bf8e043b29837739ebaea235a
medhatjachour <53466991+medhatjachour@users.noreply.github.com>
2024-07-28 04:21:40 +0300

    init new

---
+++ src/components/ProductsView.jsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+const ProductsView = () => {
+  return (
+    <div>ProductsView</div>
+  )
+}
+
+export default ProductsView
